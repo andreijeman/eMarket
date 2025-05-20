2764b870f06b093f98dfa4fb9ed89dee74524328
andreijeman <andrei.jeman3@gmail.com>
2025-05-20 14:21:09 +0300

    Implement web

---
+++ eMarket.Web/Pages/AllProducts.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using eMarket.Application.DTOs.Product;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace eMarket.Web.Pages;
+
+public class AllProducts : PageModel
+{
+    private readonly HttpClient _client;
+    private readonly IConfiguration _configuration;
+    
+    public List<ProductListDto> Products { get; set; } = new List<ProductListDto>();
+    
+    public AllProducts(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+    {
+        _client = httpClientFactory.CreateClient();
+        _configuration = configuration;
+    }
+
+    public async Task OnGetAsync()
+    {
+        var response = await _client
+            .GetFromJsonAsync<List<ProductListDto>>($"{_configuration["Api:BaseUrl"]}/Product/range?skip=0&take=9999");
+        
+        if(response != null) Products = response; 
+    }
+}
---
+++ eMarket.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace eMarket.Web.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
